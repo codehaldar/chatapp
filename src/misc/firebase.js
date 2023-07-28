9dfae315931478550d868ce87bf6c6b23ea9bbd4
codehaldar <subhamhaldar2001@gmail.com>
2023-07-28 13:20:42 +0530

    firebase added

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCwa3ACCnV05s3gTkFi7AfEDrD9yiTM3fc',
+  authDomain: 'chat-app-3978b.firebaseapp.com',
+  databaseURL: 'https://chat-app-3978b-default-rtdb.firebaseio.com',
+  projectId: 'chat-app-3978b',
+  storageBucket: 'chat-app-3978b.appspot.com',
+  messagingSenderId: '750007106101',
+  appId: '1:750007106101:web:a81be82c773996bfa60f7c',
+};
